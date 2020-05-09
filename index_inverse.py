3654c2d3e46fd57bcb09de8fcb5aad9bda8d5959
Yacine Belmaati Cherkaoui <yacine@Host-001.lan>
2020-05-09 14:08:36 +0200

    Classe index_inverse + calcul tf_idf

---
+++ index_inverse.py
@@ -0,0 +1,129 @@
+import os
+import sys
+import json as js
+import math
+import re
+import bs4
+
+class index_inverse ():
+    webpages_dir = "pages_web"
+    debug = True
+
+    regex_head = "<(\s|\t)*(head|HEAD)(.*)>(.*)<(\s|\t)*/(\s|\t)*(head|HEAD)>"
+    regex_script = "<(\s|\t)*(script|SCRIPT)(.*)>(.*)<(\s|\t)*/(\s|\t)*(script|SCRIPT)(\s|\t)*>"
+    regex_balise_close = "</(.*)>"
+    regex_balise_orphan = "<(.*)/>"
+    regex_balise = "<(.*)>"
+
+    corpus_size = 0
+    keyword_corpus = {}
+    tf_corpus = {}
+    tf_idf_corpus = {}
+
+    current_dir = os.getcwd()
+    file_list = None
+    def __init__ (self, directory = "pages_web", score ="tf-idf"):
+        self.webpages_dir = directory
+        self.file_list = os.listdir(self.webpages_dir)
+        self.score = score
+
+
+    def compute_tf(self):
+        regex_head = "<(\s|\t)*(head|HEAD)(.*)>(.*)<(\s|\t)*/(\s|\t)*(head|HEAD)>"
+        regex_script = "<(\s|\t)*(script|SCRIPT)(.*)>(.*)<(\s|\t)*/(\s|\t)*(script|SCRIPT)(\s|\t)*>"
+        regex_balise_close = "</(.*)>"
+        regex_balise_orphan = "<(.*)/>"
+        regex_balise = "<(.*)>"
+
+        os.chdir(self.webpages_dir)
+
+        for file_name in self.file_list :
+            working_file = open(file_name, "r", errors = "replace")
+            working_tf = {}
+            is_present = {}
+            corpus_size_buffer = self.corpus_size
+            ignore_file = False
+
+            if self.debug :
+                print(file_name)
+                print("traitement")
+
+            try:
+                working_string = bs4.BeautifulSoup(working_file).text
+                self.corpus_size = self.corpus_size + 1
+                working_string = re.sub("\(|\)|\[|\]|,|\."," ",working_string)
+                working_string = working_string.replace("\n"," ")
+
+
+                working_split = working_string.split(" ")
+
+                if False :
+                    print(working_string)
+                    print(working_split)
+
+                keyword_number = len(working_split)
+
+            except OSError as e:
+                self.corpus_size = corpus_size_buffer
+                working_split = []
+                ignore_file = True
+                tf_corpus.pop(file_name, None)
+
+                if self.debug :
+                    print("Erreur avec le fichier "+file_name+" \n")
+                    print("Ce fichier ne sera pris en compte dans le calcul de tf-idf")
+
+            if not ignore_file :
+                for keyword in working_split :
+
+                    if keyword in is_present :
+                        pass
+                    else :
+                        is_present[keyword] = True
+                        if keyword in self.keyword_corpus :
+                            self.keyword_corpus[keyword] += 1
+                        else :
+                            self.keyword_corpus[keyword] = 1
+
+                    if keyword in working_tf :
+                        working_tf[keyword] += 1
+                    else :
+                        working_tf[keyword] = 1
+
+                for k in working_tf :
+                    working_tf[k] =working_tf[k]/keyword_number
+                    if not (k in self.tf_corpus):
+                        self.tf_corpus[k]={}
+                    self.tf_corpus[k][file_name] = working_tf[k]
+
+
+                working_file.close()
+
+        os.chdir(self.current_dir)
+
+    def compute_tf_idf(self):
+        if self.score == "tf-idf":
+            for keyword in self.tf_corpus:
+
+                if self.debug :
+                    print(keyword)
+
+                self.tf_idf_corpus[keyword] ={}
+
+                for file in self.tf_idf_corpus[keyword]:
+
+                    if self.debug:
+                        print(file)
+
+                    idf_keyword = math.log(self.corpus_size/self.keyword_corpus[keyword])
+                    #self.tf_idf_corpus[keyword][file] = self.tf_corpus[keyword][file]*idf_keyword
+            #try:
+            #    for keyword in self.tf_corpus:
+            #        self.tf_idf_corpus[keyword] ={}
+            #        for file in keyword:
+            #            idf_keyword = math.log(corpus_size/keyword_corpus[keyword])
+            #            self.tf_idf_corpus[keyword][file] = self.tf_corpus[keyword][file]*idf_keyword
+            #except NameError :
+                #print("executez la méthode compute_tf avant")
+            #except ValueError :
+                #print("executez la méthode compute_tf avant")
---
+++ test.py
@@ -0,0 +1,5 @@
+import index_inverse as ii
+
+index = ii.index_inverse()
+index.compute_tf()
+index.compute_tf_idf()
